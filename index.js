ffa699bf57d46b754d816814f5be2a93508bf537
codewithshamim <shamimislamonline@gmail.com>
2023-02-19 22:17:27 +0600

    ssl test

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.port || 5000;
+const sslRouter = require("./routes/ssl.route");
 
 app.get("/", (req, res) => {
   res.send("Working");
 });
+
+app.use("/api/v1/ssl", sslRouter);
 
 app.listen(port, () => {
   console.log("Listening to port", port);
